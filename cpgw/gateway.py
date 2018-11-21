af1f5cdb6ae6cf3efd606df9a115b42cf1728c1e
Karel Blavka <karel.blavka@bigclown.com>
2018-11-21 06:38:46 +0100

    Update gateway

--- cpgw/gateway.py
+++ cpgw/gateway.py
@@ -8,6 +8,8 @@ import re
 import logging
 import serial
 import decimal
+import platform
+from ctypes import *
 from threading import Condition, Lock, Thread, Event
 try:
     import fcntl
@@ -40,6 +42,7 @@ items = (
 class Gateway:
 
     def __init__(self, device):
+        self._ser = None
         self._device = device
         self.on_line = None
         self.on_recv = None
@@ -51,11 +54,8 @@ class Gateway:
         logging.info("Connecting on device %s", self._device)
         self._ser = serial.Serial(self._device, baudrate=115200, timeout=3)
 
-        if fcntl and self._ser:
-            try:
-                fcntl.flock(self._ser.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
-            except Exception as e:
-                raise Exception('Could not lock device %s' % self._device)
+        self._lock()
+        self._speed_up()
 
         logging.info("Success connect on device %s", self._device)
 
@@ -63,8 +63,17 @@ class Gateway:
         self._ser.reset_input_buffer()
         self._ser.reset_output_buffer()
         time.sleep(0.5)
+        self._ser.write(b'\x1b')
 
         self.is_run = False
+
+    def __del__(self):
+        self._unlock()
+        try:
+            self._ser.close()
+        except Exception as e:
+            pass
+        self._ser = None
 
     def run(self):
         self.is_run = True
@@ -129,11 +138,66 @@ class Gateway:
             return response
 
     def get_cgsn(self):
-        response = self.command("+CGSN")
-        if not response:
-            raise Exception("Command AT+CGSN not work.")
-        return response[0].split(':')[1].strip()
+        for i in range(3):
+            response = self.command("+CGSN")
+            if not response:
+                time.sleep(0.5)
+                continue
+            return response[0].split(':')[1].strip()
+        raise Exception("Command AT+CGSN not work.")
 
     def start(self):
         """Run in thread"""
         Thread(target=self.run, args=[]).start()
+
+    def _lock(self):
+        if fcntl or not self._ser:
+            return
+        try:
+            fcntl.flock(self._ser.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except Exception as e:
+            raise Exception('Could not lock device %s' % self._device)
+
+    def _unlock(self):
+        if not fcntl or not self._ser:
+            return
+        fcntl.flock(self._ser.fileno(), fcntl.LOCK_UN)
+
+    def _speed_up(self):
+        if not fcntl:
+            return
+        if platform.system() != 'Linux':
+            return
+
+        TIOCGSERIAL = 0x0000541E
+        TIOCSSERIAL = 0x0000541F
+        ASYNC_LOW_LATENCY = 0x2000
+
+        class serial_struct(Structure):
+            _fields_ = [("type", c_int),
+                        ("line", c_int),
+                        ("port", c_uint),
+                        ("irq", c_int),
+                        ("flags", c_int),
+                        ("xmit_fifo_size", c_int),
+                        ("custom_divisor", c_int),
+                        ("baud_base", c_int),
+                        ("close_delay", c_ushort),
+                        ("io_type", c_byte),
+                        ("reserved_char", c_byte * 1),
+                        ("hub6", c_uint),
+                        ("closing_wait", c_ushort),
+                        ("closing_wait2", c_ushort),
+                        ("iomem_base", POINTER(c_ubyte)),
+                        ("iomem_reg_shift", c_ushort),
+                        ("port_high", c_int),
+                        ("iomap_base", c_ulong)]
+
+        buf = serial_struct()
+
+        try:
+            fcntl.ioctl(self._ser.fileno(), TIOCGSERIAL, buf)
+            buf.flags |= ASYNC_LOW_LATENCY
+            fcntl.ioctl(self._ser.fileno(), TIOCSSERIAL, buf)
+        except Exception as e:
+            pass
